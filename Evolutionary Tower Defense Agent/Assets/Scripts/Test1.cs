42b2620ce6b51a66857b69e0e2e9429592fc7647
Tibor Fekete <tiborfek7@gmail.com>
2021-11-03 15:46:33 +0100

    Testing the Simulator dll

--- Evolutionary Tower Defense Agent/Assets/Scripts/Test1.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/Test1.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Test1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    Simulator.SimulatorService service;
     void Start()
     {
-        
+        service = new Simulator.SimulatorService();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(service.CurrentStep());
     }
 }
