61596b9296ba4ec7044c9616f8030fd66a01cf31
Elod Vizi <elod.vizi@yahoo.com>
2021-11-03 15:21:08 +0100

    removed Test added Test1

--- Evolutionary Tower Defense Agent/Assets/Scripts/Test1.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/Test1.cs
@@ -1,12 +1,13 @@
-using Simulator;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
