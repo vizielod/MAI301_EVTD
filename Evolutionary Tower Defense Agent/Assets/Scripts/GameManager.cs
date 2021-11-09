cba070bc4faac99244230f8044622361ea5fde0b
Elod Vizi <elod.vizi@yahoo.com>
2021-11-09 18:43:49 +0100

    Started to implement Grid class

--- Evolutionary Tower Defense Agent/Assets/Scripts/GameManager.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Grid grid = new Grid(5, 5, 5);
     }
 
     // Update is called once per frame
---
+++ Evolutionary Tower Defense Agent/Assets/Scripts/Grid.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    private float cellSize;
+
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int i = 0; i < gridArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < gridArray.GetLength(1); j++)
+            {
+                Debug.Log(i + ", " + j);
+            }
+        }
+    }
+}
