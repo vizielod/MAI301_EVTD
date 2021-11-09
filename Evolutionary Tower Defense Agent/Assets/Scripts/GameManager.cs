53f55f53b60cbff7f5e2f9010ec34d216ad2bc25
Elod Vizi <elod.vizi@yahoo.com>
2021-11-09 23:57:56 +0100

    Initializing Tower Defense Map layout from code using 2D array of ints

--- Evolutionary Tower Defense Agent/Assets/Scripts/GameManager.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/GameManager.cs
@@ -2,18 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    Ground = 0,
+    Wall = 1,
+    Spawn = 2,
+    Goal = 3
+}
+
 public class GameManager : MonoBehaviour
 {
+    public GameObject Wall;
+    public GameObject Ground;
+    public GameObject Spawn;
+    public GameObject Goal;
 
     public GameObject enemy;
+    private Grid grid;
+
+    public int[,] gridArray = new int[,]
+        {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            { 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 3, 1},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        };
+
+    public TileType[,] tileTypeArray;
+
+    void SetTileTypeArray()
+    {
+        tileTypeArray = new TileType[gridArray.GetLength(0), gridArray.GetLength(1)];
+        for (int i = 0; i < gridArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < gridArray.GetLength(1); j++)
+            {
+                tileTypeArray[i, j] = (TileType)gridArray[i, j];
+            }
+        }
+    }
     private void Awake()
     {
-        //enemies = new GameObject[]
     }
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(5, 5, 5);
+        SetTileTypeArray();
+        grid = new Grid(tileTypeArray.GetLength(0), tileTypeArray.GetLength(1), 5, tileTypeArray); // int rowsOrHeight = ary.GetLength(0); int colsOrWidth = ary.GetLength(1);
+        InitializeGridTiles();
     }
 
     // Update is called once per frame
@@ -38,4 +85,79 @@ public class GameManager : MonoBehaviour
     {
         enemy.GetComponent<EnemyMovementController>().PerformStepBackward();
     }
+
+    void InitializeGridTiles()
+    {
+        if (grid == null)
+            return;
+
+        for (int i = 0; i < grid.Width; i++)
+        {
+            for (int j = 0; j < grid.Height; j++)
+            {
+                Debug.Log(grid.TypeAt(i, j));
+                InstantiateGridTile(grid.TypeAt(i, j), i, j);
+            }
+        }
+    }
+
+    void InstantiateGridTile(TileType tileType, int i, int j)
+    {
+        GameObject tile;
+        Transform parent;
+
+        switch (tileType)
+        {
+            case TileType.Ground:
+                tile = Ground;
+                parent = transform.Find("Ground");
+                break;
+            case TileType.Spawn:
+                tile = Spawn;
+                parent = transform;
+                break;
+            case TileType.Goal:
+                tile = Goal;
+                parent = transform;
+                break;
+            case TileType.Wall:
+                tile = Wall;
+                parent = transform.Find("Tiles");
+                break;
+            default:
+                tile = Wall;
+                parent = transform.Find("Tiles");
+                break;
+        }
+
+        Transform newTile = (Instantiate(tile, new Vector3(i * grid.cellSize, 0, j * grid.cellSize), Quaternion.identity) as GameObject).transform;
+
+        newTile.SetParent(parent);
+    }
+
+    /*void OnDrawGizmos()
+    {
+        if(grid == null)
+        {
+            return;
+        }
+        for (int i = 0; i < grid.gridArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.gridArray.GetLength(1); j++)
+            {
+                var debug_line_horizontal_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_horizontal_end = new Vector3(GetWorldPosition(i, j + 1).x - cellSize / 2, GetWorldPosition(i, j + 1).y, GetWorldPosition(i, j + 1).z - cellSize / 2);
+                var debug_line_vertical_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_vertical_end = new Vector3(GetWorldPosition(i + 1, j).x - cellSize / 2, GetWorldPosition(i + 1, j).y, GetWorldPosition(i + 1, j).z - cellSize / 2);
+                Debug.DrawLine(debug_line_horizontal_start, debug_line_horizontal_end, Color.white, 100f);
+                Debug.DrawLine(debug_line_vertical_start, debug_line_vertical_end, Color.white, 100f);
+            }
+        }
+        var debug_last_line_horizontal_start = new Vector3(GetWorldPosition(0, height).x - cellSize / 2, GetWorldPosition(0, height).y, GetWorldPosition(0, height).z - cellSize / 2);
+        var debug_last_line_horizontal_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        var debug_last_line_vertical_start = new Vector3(GetWorldPosition(width, 0).x - cellSize / 2, GetWorldPosition(width, 0).y, GetWorldPosition(width, 0).z - cellSize / 2);
+        var debug_last_line_vertical_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        Debug.DrawLine(debug_last_line_horizontal_start, debug_last_line_horizontal_end, Color.white, 100f);
+        Debug.DrawLine(debug_last_line_vertical_start, debug_last_line_vertical_end, Color.white, 100f);
+    }*/
 }
--- Evolutionary Tower Defense Agent/Assets/Scripts/Grid.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/Grid.cs
@@ -2,28 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*public enum TileType
+{
+    Ground,
+    Wall,
+    Spawn,
+    Goal
+}*/
+
 public class Grid
 {
-    private int width;
-    private int height;
-    private int[,] gridArray;
 
-    private float cellSize;
+    public int Width { get; }
+    public int Height { get; }
 
-    public Grid(int width, int height, float cellSize)
+    public float cellSize;
+
+    public TileType[,] tileTypeArray;
+
+    public Grid(int height, int width, float cellSize, TileType[,] tileTypeArray)
     {
-        this.width = width;
-        this.height = height;
+        this.Height = height;
+        this.Width = width;
         this.cellSize = cellSize;
 
-        gridArray = new int[width, height];
+        this.tileTypeArray = tileTypeArray;
+        /*
+        tileTypeArray = new TileType[, ]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };*/
 
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                //this.tileType = TileType.Ground;
+                //tileTypeArray[i, j] = TileType.Ground;
+                //Debug.Log("Center of cell: " + i + ", " + j + " is world position: " + GetWorldPosition(i, j) + " TileType: " + tileTypeArray[i, j]);
+
+                var debug_line_horizontal_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_horizontal_end = new Vector3(GetWorldPosition(i, j + 1).x - cellSize / 2, GetWorldPosition(i, j + 1).y, GetWorldPosition(i, j + 1).z - cellSize / 2);
+                var debug_line_vertical_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_vertical_end = new Vector3(GetWorldPosition(i + 1, j).x - cellSize / 2, GetWorldPosition(i + 1, j).y, GetWorldPosition(i + 1, j).z - cellSize / 2);
+                Debug.DrawLine(debug_line_horizontal_start, debug_line_horizontal_end, Color.white, 100f);
+                Debug.DrawLine(debug_line_vertical_start, debug_line_vertical_end, Color.white, 100f);
+            }
+        }
+        var debug_last_line_horizontal_start = new Vector3(GetWorldPosition(0, height).x - cellSize / 2, GetWorldPosition(0, height).y, GetWorldPosition(0, height).z - cellSize / 2);
+        var debug_last_line_horizontal_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        var debug_last_line_vertical_start = new Vector3(GetWorldPosition(width, 0).x - cellSize / 2, GetWorldPosition(width, 0).y, GetWorldPosition(width, 0).z - cellSize / 2);
+        var debug_last_line_vertical_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        Debug.DrawLine(debug_last_line_horizontal_start, debug_last_line_horizontal_end, Color.white, 100f);
+        Debug.DrawLine(debug_last_line_vertical_start, debug_last_line_vertical_end, Color.white, 100f);
+
+    }
+
+    public Vector3 GetWorldPosition(int i, int j)
+    {
+        return new Vector3(i, 0, j) * cellSize;
+    }
+
+    public TileType TypeAt(int i, int j)
+    {
+        return tileTypeArray[i, j];
+    }
+
+    /*void OnDrawGizmos()
+    {
         for (int i = 0; i < gridArray.GetLength(0); i++)
         {
             for (int j = 0; j < gridArray.GetLength(1); j++)
             {
-                Debug.Log(i + ", " + j);
+                //Debug.Log("Center of cell: " + i + ", " + j + " is world position: " + GetWorldPosition(i, j));
+                var debug_line_horizontal_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_horizontal_end = new Vector3(GetWorldPosition(i, j + 1).x - cellSize / 2, GetWorldPosition(i, j + 1).y, GetWorldPosition(i, j + 1).z - cellSize / 2);
+                var debug_line_vertical_start = new Vector3(GetWorldPosition(i, j).x - cellSize / 2, GetWorldPosition(i, j).y, GetWorldPosition(i, j).z - cellSize / 2);
+                var debug_line_vertical_end = new Vector3(GetWorldPosition(i + 1, j).x - cellSize / 2, GetWorldPosition(i + 1, j).y, GetWorldPosition(i + 1, j).z - cellSize / 2);
+                Debug.DrawLine(debug_line_horizontal_start, debug_line_horizontal_end, Color.white, 100f);
+                Debug.DrawLine(debug_line_vertical_start, debug_line_vertical_end, Color.white, 100f);
             }
         }
-    }
+        var debug_last_line_horizontal_start = new Vector3(GetWorldPosition(0, height).x - cellSize / 2, GetWorldPosition(0, height).y, GetWorldPosition(0, height).z - cellSize / 2);
+        var debug_last_line_horizontal_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        var debug_last_line_vertical_start = new Vector3(GetWorldPosition(width, 0).x - cellSize / 2, GetWorldPosition(width, 0).y, GetWorldPosition(width, 0).z - cellSize / 2);
+        var debug_last_line_vertical_end = new Vector3(GetWorldPosition(width, height).x - cellSize / 2, GetWorldPosition(width, height).y, GetWorldPosition(width, height).z - cellSize / 2);
+        Debug.DrawLine(debug_last_line_horizontal_start, debug_last_line_horizontal_end, Color.white, 100f);
+        Debug.DrawLine(debug_last_line_vertical_start, debug_last_line_vertical_end, Color.white, 100f);
+    }*/
 }
