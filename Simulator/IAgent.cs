85ac938116aaaa230d6e950f39c98cc3c91a4a8b
Elod Vizi <elod.vizi@yahoo.com>
2021-11-14 15:46:25 +0100

    Attemt to fix the prefab bug. Created Turret prefab

--- Simulator/IAgent.cs
+++ Simulator/IAgent.cs
@@ -5,7 +5,7 @@ namespace Simulator
     public interface IAgent
     {
         IAction PickAction(IState state);
-        (int x, int y) InitialPosition { get; }
+        //(int x, int y) InitialPosition { get; }
 
     }
 }
