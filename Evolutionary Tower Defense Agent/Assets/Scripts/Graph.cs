19f9934bffaf65fb9ab82f77052085173ce2113c
Elod Vizi <elod.vizi@yahoo.com>
2021-12-16 22:57:49 +0100

    Graph visualization improvements. Connecting dots and printing score

--- Evolutionary Tower Defense Agent/Assets/Scripts/Graph.cs
+++ Evolutionary Tower Defense Agent/Assets/Scripts/Graph.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using CodeMonkey.Utils;
 
 public class Graph : MonoBehaviour
 {
@@ -20,17 +21,17 @@ public class Graph : MonoBehaviour
         Debug.Log(this.transform.GetComponent<RectTransform>().rect.height);
         graphContainer.sizeDelta = new Vector2(this.transform.GetComponent<RectTransform>().rect.width,
             this.transform.GetComponent<RectTransform>().rect.height);
-        graphContainer.position = new Vector3(this.transform.GetComponent<RectTransform>().rect.width / 2, this.transform.GetComponent<RectTransform>().rect.height / 2, 0);
-
+        //graphContainer.position = new Vector3(this.transform.GetComponent<RectTransform>().rect.width / 2, this.transform.GetComponent<RectTransform>().rect.height / 2, 0);
+        graphContainer.position = new Vector3(0, 0, 0);
     }
 
-    private void Update()
+    /*private void Update()
     {
         //TODO: Instead of Update, just make a last adjustment when the Start Button is clicked
         graphContainer.sizeDelta = new Vector2(this.transform.GetComponent<RectTransform>().rect.width,
             this.transform.GetComponent<RectTransform>().rect.height);
         graphContainer.position = new Vector3(this.transform.GetComponent<RectTransform>().rect.width / 2, this.transform.GetComponent<RectTransform>().rect.height / 2, 0);
-    }
+    }*/
 
     public void addValue(float value)
     {
@@ -38,16 +39,38 @@ public class Graph : MonoBehaviour
         ShowGraph(valueList);
     }
 
-    private void CreateCircle(Vector2 anchoredPosition)
+    private GameObject CreateCircle(Vector2 anchoredPosition, float scoreValue)
     {
-        GameObject go = new GameObject("circle", typeof(Image));
-        go.transform.SetParent(graphContainer, false);
-        go.GetComponent<Image>().sprite = circleSprite;
-        RectTransform rectTransform = go.GetComponent<RectTransform>();
+        GameObject circleGO = new GameObject("circle", typeof(Image));
+        circleGO.transform.SetParent(graphContainer, false);
+        circleGO.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = circleGO.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = anchoredPosition;
         rectTransform.sizeDelta = new Vector2(8, 8);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
+        CreateText(circleGO, scoreValue);
+        return circleGO;
+    }
+
+    private void CreateText(GameObject circleGO, float scoreValue)
+    {
+        GameObject scoreTextGO = new GameObject("scoreText", typeof(Text));
+        scoreTextGO.transform.SetParent(circleGO.transform, false);
+
+        Text textComponent = scoreTextGO.GetComponent<Text>();
+        textComponent.text = scoreValue.ToString();
+
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        textComponent.font = ArialFont;
+        textComponent.material = ArialFont.material;
+
+        textComponent.alignment = TextAnchor.MiddleCenter;
+
+        RectTransform rectTransform = scoreTextGO.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 20, 0);
+        rectTransform.sizeDelta = new Vector2(46, 20);
+
     }
 
     private void ShowGraph(List<float> valueList)
@@ -64,14 +87,41 @@ public class Graph : MonoBehaviour
         float yMaximum = 20000f;
         float xSize = graphWidth / valueList.Count;
         float xMargin = xSize / 2;
+
+        GameObject lastCircleGO = null;
         for (int i = 0; i < valueList.Count; i++)
         {
             float xPosition = i * xSize + xMargin;
             float yPosition = (valueList[i] / yMaximum) * (graphHeight - 2 * yMargin) + yMargin;
-            CreateCircle(new Vector2(xPosition, yPosition));
+            GameObject circleGO = CreateCircle(new Vector2(xPosition, yPosition), valueList[i]);
+            if(lastCircleGO != null)
+            {
+                /*CreateDotConnection(lastCircleGO.GetComponent<RectTransform>().anchoredPosition,
+                    circleGO.GetComponent<RectTransform>().anchoredPosition);*/
+                CreateDotConnection(lastCircleGO.GetComponent<RectTransform>().position, 
+                    circleGO.GetComponent<RectTransform>().position);
+            }
+            lastCircleGO = circleGO;
         }
     }
 
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.rotation = Quaternion.Euler(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+
+        //rectTransform.rotation = 
+        //rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+    }
     private void Clean()
     {
         foreach (Transform child in graphContainer.transform)
