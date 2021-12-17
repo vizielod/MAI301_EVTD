d75d8de165bc477dd012a375fd0a0185ed421791
Thomas Volden <tv@chargetime.eu>
2021-12-17 18:49:31 +0100

    Added generation info for callback

---
+++ Evolution/GenerationInfo.cs
@@ -0,0 +1,19 @@
+﻿using Simulator;
+
+namespace Evolution
+{
+    public class GenerationInfo
+    {
+        public float Score { get; }
+        public int Generation { get; }
+        public IStateSequence Simulation { get; }
+
+        public GenerationInfo(int generation, float score, IStateSequence simulation)
+        {
+            Generation = generation;
+            Score = score;
+            Simulation = simulation;
+        }
+
+    }
+}
